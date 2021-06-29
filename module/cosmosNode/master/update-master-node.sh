54f1ff200fad087b1d57410a32581f3cb96dbe9a
sunny960 <sunnypardhan80@gmail.com>
2021-06-29 19:23:21 +0530

    provide config

--- module/cosmosNode/master/update-master-node.sh
+++ module/cosmosNode/master/update-master-node.sh
@@ -5,6 +5,10 @@ GRAVITY_GENESIS_FILE="/root/.gravity/config/genesis.json"
 BUCKET_MASTER_GENESIS_FILE="master/genesis.json"
 PEER_INFO="peerInfo"
 
+GIT_HUB_USER=$1
+GIT_HUB_PASS=$2
+GIT_HUB_EMAIL=$3
+
 echo "Get pull updates"
 RUN git pull origin main
 
@@ -27,9 +31,9 @@ cp $GRAVITY_GENESIS_FILE $BUCKET_MASTER_GENESIS_FILE
 echo "git add command"
 git add .
 echo "git add git config command"
-git config --global user.email "sunnyk@leewayhertz.com"
-git config --global user.name "sunnyk56"
-git remote set-url origin https://sunnyk56:Leeway321@github.com/sunnyk56/gravity-bridge.git
+git config --global user.email $GIT_HUB_EMAIL
+git config --global user.name $GIT_HUB_USER
+git remote set-url origin https://$GIT_HUB_USER:$GIT_HUB_PASS@github.com/sunnyk56/gravity-bridge.git
 echo "git commit command"
 git commit -m "add genesis file"
 echo "git push command"
