d68023e7f7bc283b58a1a1dc68e9232b2b073e8a
sunny960 <sunnypardhan80@gmail.com>
2021-06-30 17:37:29 +0530

    add peer information

---
+++ module/cosmosNode/peerNode/start.sh
@@ -0,0 +1,51 @@
+GENTEX_FILE="/root/.gravity/config/gentx/."
+VALIDATOR_FILE="/root/validator2.json"
+BUCKET_MASTER_GENTEX_FILE="peerInfo/gentx"
+BUCKET_MASTER_VALIDATOR_FILE="peerInfo/validator.json"
+
+GIT_HUB_USER=$1
+GIT_HUB_PASS=$2
+GIT_HUB_EMAIL=$3
+GIT_HUB_BRANCH=$4
+
+
+
+echo "Get pull updates"
+git pull origin $GIT_HUB_BRANCH
+
+
+
+echo "Gentx file moving"
+rm -r peerInfo
+mkdir peerInfo
+mkdir peerInfo/gentx
+touch $BUCKET_MASTER_VALIDATOR_FILE
+
+
+
+echo "Copying validator file"
+cp $VALIDATOR_FILE $BUCKET_MASTER_VALIDATOR_FILE
+
+
+
+echo "Copying gentx file"
+cp -R $GENTEX_FILE $BUCKET_MASTER_GENTEX_FILE
+
+
+
+echo "git add command"
+git add .
+echo "git add git config command"
+
+git config --global user.email $GIT_HUB_EMAIL
+git config --global user.name $GIT_HUB_USER
+git remote set-url origin https://$GIT_HUB_USER:$GIT_HUB_PASS@github.com/sunnyk56/gravity-bridge.git
+
+echo "git commit command"
+git commit -m "added peerinfo folder"
+
+echo "git fetch command"
+git fetch
+
+echo "git push command"
+git push origin $GIT_HUB_BRANCH
---
+++ module/cosmosNode/peerNode/update-peer-node.sh
@@ -0,0 +1,23 @@
+
+GRAVITY_GENESIS_FILE="/root/.gravity/config/genesis.json"
+GRAVITY_CONFIG_FILE="/root/.gravity/config/config.toml"
+BUCKET_MASTER_GENESIS_FILE="/root/mainNode/master/genesis.json"
+BUCKET_MASTER_SEED_FILE="/root/mainNode/master/seed"
+MAIN_NODE="/root/mainNode"
+
+
+echo "Get pull updates"
+git clone -b config https://github.com/sunnyk56/gravity-bridge.git $MAIN_NODE
+
+echo "Copying genesis file"
+cp $BUCKET_MASTER_GENESIS_FILE  $GRAVITY_GENESIS_FILE
+peerseed=$(cat $BUCKET_MASTER_SEED_FILE)
+
+sed -i 's#persistent_peers = ""#persistent_peers = "'$peerseed'"#g' $GRAVITY_CONFIG_FILE
+
+rm -r $MAIN_NODE
+
+# Resets the blockchain database, removes address book files and start the node
+gravity unsafe-reset-all
+gravity --home /root/.gravity/ --address tcp://0.0.0.0:26655 --rpc.laddr tcp://0.0.0.0:26657 --grpc.address 0.0.0.0:9090 --log_level error --p2p.laddr tcp://0.0.0.0:26656 --rpc.pprof_laddr 0.0.0.0:6060 start
+
