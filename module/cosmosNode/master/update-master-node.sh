7a0842d35988c86badfd3b7229d6bae62c609c2b
sunny960 <sunnypardhan80@gmail.com>
2021-06-30 19:08:24 +0530

    add peer information5

--- module/cosmosNode/master/update-master-node.sh
+++ module/cosmosNode/master/update-master-node.sh
@@ -1,7 +1,7 @@
 GRAVITY_CONFIG_FILE="/root/.gravity/config"
 GRAVITY_GENESIS_FILE="/root/.gravity/config/genesis.json"
 BUCKET_MASTER_GENESIS_FILE="master/genesis.json"
-PEER_INFO="peerInfo"
+PEER_INFO="/root/gravity-bridge/peerInfo"
 
 GIT_HUB_USER=$1
 GIT_HUB_PASS=$2
@@ -40,5 +40,5 @@ echo "git push command"
 git push origin $GIT_HUB_BRANCH
 
 # Resets the blockchain database, removes address book files and start the node
-gravity unsafe-reset-all
-gravity --home /root/.gravity/ --address tcp://0.0.0.0:26655 --rpc.laddr tcp://0.0.0.0:26657 --grpc.address 0.0.0.0:9090 --log_level error --p2p.laddr tcp://0.0.0.0:26656 --rpc.pprof_laddr 0.0.0.0:6060 start
+#gravity unsafe-reset-all
+#gravity --home /root/.gravity/ --address tcp://0.0.0.0:26655 --rpc.laddr tcp://0.0.0.0:26657 --grpc.address 0.0.0.0:9090 --log_level error --p2p.laddr tcp://0.0.0.0:26656 --rpc.pprof_laddr 0.0.0.0:6060 start
