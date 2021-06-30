f6a05aa048901b6174e0d761ff72db3d9609883a
shivaLeeway <86414335+shivaLeeway@users.noreply.github.com>
2021-06-30 12:09:53 +0530

    Update start.sh

--- module/cosmosNode/peerNode/start.sh
+++ module/cosmosNode/peerNode/start.sh
@@ -3,6 +3,11 @@ VALIDATOR_FILE="/root/validator2.json"
 BUCKET_MASTER_GENTEX_FILE="peerInfo/gentx"
 BUCKET_MASTER_VALIDATOR_FILE="peerInfo"
 
+GIT_HUB_USER=$1
+GIT_HUB_PASS=$2
+GIT_HUB_EMAIL=$3
+GIT_REPO=$4
+
 echo "Gentx file moving"
 rm -r peerInfo
 mkdir peerInfo
@@ -18,11 +23,11 @@ echo "git add command"
 git add .
 
 echo "git add git config command"
-git config --global user.email "shiva@leewayhertz.com"
-git config --global user.name "shivaLeeway"
+git config --global user.email $GIT_HUB_EMAIL
+git config --global user.name $GIT_HUB_USER
 
 echo "git commit command"
 git commit -m "Gentx moved"
 
 echo "git push command"
-git push https://shivaLeeway:leeway%40123@github.com/shivaLeeway/gravity-bridge.git
+git push https://$GIT_HUB_USER:$GIT_HUB_PASS@$GIT_REPO.git
